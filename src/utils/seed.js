e4d130cac0ae512a1c9dfd32756085128efa89c6
Maciej Wojsław <maciej.wojslaw@espeo.eu>
2018-08-11 14:28:31 +0200

    directly set _id while seeding.

--- src/utils/seed.js
+++ src/utils/seed.js
@@ -1,9 +1,11 @@
 const seedMovies = db =>
   db.collection("movie").insertMany([
     {
+      _id: 1,
       title: "Harry Potter and the philosopher's stone"
     },
     {
+      _id: 2,
       title: "Harry Potter and the chamber of secrets"
     }
   ]);
